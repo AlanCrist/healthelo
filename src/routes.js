18b1b2bfc016f661b0c8f84c106ea802228ae338
Alan <lanzn16@outlook.com>
2020-03-28 18:37:50 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,23 @@
 const express = require("express");
 
 const HospitalController = require("./controllers/HospitalController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const connection = require("./database/connection");
 
 const routes = express.Router();
 
-routes.get("/hospitais", HospitalController.index);
+routes.post("/sessions", SessionController.create);
 
+routes.get("/hospitais", HospitalController.index);
 routes.post("/hospitais", HospitalController.create);
+
+routes.get("/profile", ProfileController.index);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = routes;

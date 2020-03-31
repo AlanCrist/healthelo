19a52d79c16c9951851efa162e253122b7c99841
Alan <lanzn16@outlook.com>
2020-03-30 21:27:34 -0300

    add routes validations

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,79 @@
 const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
 
 const HospitalController = require("./controllers/HospitalController");
 const IncidentController = require("./controllers/IncidentController");
 const ProfileController = require("./controllers/ProfileController");
 const SessionController = require("./controllers/SessionController");
 
-const connection = require("./database/connection");
-
 const routes = express.Router();
 
 routes.post("/sessions", SessionController.create);
 
 routes.get("/hospitais", HospitalController.index);
-routes.post("/hospitais", HospitalController.create);
+routes.post(
+  "/hospitais",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  HospitalController.create
+);
 
-routes.get("/profile", ProfileController.index);
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
 
-routes.get("/incidents", IncidentController.index);
-routes.post("/incidents", IncidentController.create);
-routes.delete("/incidents/:id", IncidentController.delete);
+routes.get(
+  "/incidents",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      value: Joi.number().required()
+    })
+  }),
+  IncidentController.index
+);
+
+routes.post(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentController.create
+);
+routes.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number()
+    })
+  }),
+  IncidentController.delete
+);
 
 module.exports = routes;
